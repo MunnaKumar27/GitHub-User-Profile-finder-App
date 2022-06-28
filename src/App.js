8f3ef94fbdb347ef27c3b8923f8e52b6808142f1
MUNNA KUMAR <96313339+MunnaKumar27@users.noreply.github.com>
2022-06-28 10:49:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Showprofile from './Components/Showprofile';
+import './App.css';
+
+function App() {
+    return (
+        <div className='body'>
+            <Showprofile/>
+        </div>
+    )
+}
+
+export default App;
